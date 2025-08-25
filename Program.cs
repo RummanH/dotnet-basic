6306e4c2511f3a438532efdbf3cd5f1387c89e25
rummanH <jah.hasant@gmail.com>
2025-08-25 19:45:24 +0600

    Successfully application done

--- Program.cs
+++ Program.cs
@@ -7,26 +7,32 @@ using Doctors.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DoctorsDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Repository (Scoped)
+builder.Services.AddDbContext<DoctorsDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
-
-// Service (Scoped)
 builder.Services.AddScoped<DoctorService>();
-
-
-
-
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+  {
+    Title = "Doctors API",
+    Version = "v1",
+    Description = "A simple API to manage doctors"
+  });
+});
 
 
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionMiddleware>();
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.MapControllers();
 
 app.Run();
